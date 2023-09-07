5bd96353f62946e362adc9e75f5220bb6d05376a
valejandraaa <142243750+valejandraaa@users.noreply.github.com>
2023-09-06 19:27:21 -0600

    Add files via upload

---
+++ semana 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Console.WriteLine("Hola mundo");*/
+         /* Console.WriteLine("soy NOMBRE");*/
+
+         /* Console.Write("Hola Mundo");
+          Console.Write("soy Valeria");*/
+
+         /* Console.ReadKey();
+          Console.ReadLine();*/
+
+        Console.WriteLine("Ingrese su nombre: ");
+        string Nombre = Console.ReadLine();
+
+        Console.WriteLine("Hola Mundo");
+        Console.WriteLine("soy " + Nombre);
+
+        Console.Write("Hola Mundo");
+        Console.Write(" soy " + Nombre);
+        Console.ReadLine();
+
+    }
+
+}
