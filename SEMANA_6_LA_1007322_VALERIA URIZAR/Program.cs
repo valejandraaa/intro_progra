461d2991c778b784eca82048b39e24737e743810
valejandraaa <142243750+valejandraaa@users.noreply.github.com>
2023-09-13 07:46:08 -0600

    Add files via upload

---
+++ SEMANA_6_LA_1007322_VALERIA URIZAR/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace;
+    
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ejercicio no 2");
+        int numero; // EnvironmentVariableTarget tipo entero
+        string dia; //variable tipo cadena 
+
+        Console.WriteLine("Ingrese un numero del uno al siete:  ");
+        dia = Console.ReadLine();
+        numero = Int32.Parse(dia);
+
+        if (numero >= 1 && numero <= 7)
+        {
+            if (numero == 1)
+            {
+                Console.WriteLine("Lunes");
+            }
+            else if (numero == 2)
+            {
+                Console.WriteLine("Martes");
+            }
+            else if (numero == 3)
+            {
+                Console.WriteLine("Miercoles");
+            }
+            else if (numero == 4)
+            {
+                Console.WriteLine("Jueves");
+            }
+            else if (numero == 5)
+            {
+                Console.WriteLine("Viernes");
+            }
+            else if (numero == 6)
+            {
+                Console.WriteLine("Sabado");
+            }
+            else if (numero == 7)
+            {
+                Console.WriteLine("Domingo");
+            }
+            else if (numero >= 8)
+            {
+
+                Console.WriteLine("Numero invalido");
+            }
+        }
+    }
+} 
+    
