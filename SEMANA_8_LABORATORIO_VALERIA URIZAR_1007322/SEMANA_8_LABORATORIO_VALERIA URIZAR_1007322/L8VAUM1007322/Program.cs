85daf9c7af32944577ffcb2cb50244ad500a7a82
valejandraaa <142243750+valejandraaa@users.noreply.github.com>
2023-09-25 10:01:11 -0600

    Add files via upload

---
+++ SEMANA_8_LABORATORIO_VALERIA URIZAR_1007322/SEMANA_8_LABORATORIO_VALERIA URIZAR_1007322/L8VAUM1007322/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L8VAUM1007322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("laboratorio semana 8");
+            Console.WriteLine("\nIngrese un numero");
+
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int A = 0, B = 1, C = 0, I = 2;
+            String resultado = "";
+
+
+            if (N > 0)
+            {
+                resultado = A.ToString();
+                if (N > 1)
+                {
+                    resultado =  B.ToString();
+                    while (I < N)
+                    {
+                        C = A + B;
+                        resultado += C.ToString();
+                        A = B;
+                        B = C;
+                        I++; 
+                    
+
+                    }
+                    Console.WriteLine("Resultado" + resultado);
+                     
+                }
+                else
+                {
+                    Console.WriteLine("Resultado " + resultado);
+
+                }
+               
+                    
+           
+                   
+            } 
+            else
+            {
+                Console.WriteLine("Resultado " + resultado);
+
+            }
+                
+
+
+            
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
