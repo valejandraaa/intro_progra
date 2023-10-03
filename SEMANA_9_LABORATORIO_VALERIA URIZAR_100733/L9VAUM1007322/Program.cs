dde79110d93b3a84c9a19aa44122a7765bc7a80a
valejandraaa <142243750+valejandraaa@users.noreply.github.com>
2023-10-02 18:19:09 -0600

    Add files via upload

---
+++ SEMANA_9_LABORATORIO_VALERIA URIZAR_100733/L9VAUM1007322/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9VAUM1007322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            // Calcula la sumatoria desde 1 hasta el número ingresado usando un do-while
+            int sumatoria = 0;
+            int contador = 1;
+            do
+            {
+                sumatoria += contador;
+                contador++;
+            } while (contador <= numero);
+
+            Console.WriteLine("Resultado de sumatoria " + sumatoria);
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("\nTabla del " + i + ":\t");
+
+                for (int j = 1; j <= numero; j++)
+                {
+                    Console.Write(i * j + "\t");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
