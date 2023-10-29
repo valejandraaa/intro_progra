41211409153ee6053512d6291b58467478ca3204
valejandraaa <142243750+valejandraaa@users.noreply.github.com>
2023-10-29 14:45:33 -0600

    Add files via upload

---
+++ SEMANA_12_LABORATORIO_VALERIA URIZAR_1007322/SEMANA 12_LAB_1007322_VALERIA URIZAR/L12VAUM1007322/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12VAUM1007322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo objCirculo;
+
+            Console.WriteLine("Ingrese el radio del círculo: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            objCirculo = new Circulo(r);
+
+            double perimetro = 0;
+            double area = 0;
+            double volumen = 0;
+
+            objCirculo.CalcularGeometria(ref perimetro, ref area, ref volumen);
+
+            Console.WriteLine("Perímetro del círculo: " + perimetro + "cm");
+            Console.WriteLine("Área del círculo: " + area + "cm^2");
+            Console.WriteLine("Volumen del círculo: " + volumen + "cm^3");
+        }
+
+
+ 
+        public class Circulo
+        {
+            //atributos
+
+            private double radio { get; set; }
+
+            public Circulo(double r)
+            {
+                //definiendo datos por defecto
+                this.radio = r;
+            }
+
+            //función perímetro
+            private double ObtenerPerimetro(double r)
+            {
+                return 2 * Math.PI * r;
+            }
+
+            //función área
+            private double ObtenerArea(double r)
+            {
+                return Math.PI * Math.Pow(r, 2);
+            }
+
+            //función volumen
+            private double ObtenerVolumen(double r)
+            {
+                return (4 * Math.PI * Math.Pow(r, 3) / 3);
+            }
+
+            //procedimiento para Calcular Geometría
+            public void CalcularGeometria(ref double unPerimetro, ref double unArea, ref double unVolumen)
+            {
+                unPerimetro = ObtenerPerimetro(radio);
+
+                unArea = ObtenerArea(radio);
+
+                unVolumen = ObtenerVolumen(radio);
+            }
+        }
+    }
+}
---
+++ SEMANA_12_LABORATORIO_VALERIA URIZAR_1007322/T10VAUM1007322/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T10VAUM1007322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            private double catetoA;
+        private double anguloOpuestoA;
+
+
+        public TrianguloRectangulo (double catetoA, double anguloOpuestoA)
+        {
+            this.catetoA = catetoA;
+            this.anguloOpuestoA = anguloOpuestoA;
+        }
+
+        public double ObtenerCatetoA()
+        {
+            return catetoA;
+        }
+
+        public double ObtenerCatetoB()
+        {
+
+            return catetoA * Math.Tan(anguloOpuestoA * (Math.PI / 180));
+        }
+
+        public double ObtenerHipotenusa()
+        {
+            return catetoA / Math.Cos(anguloOpuestoA * (Math.PI / 180));
+        }
+
+        public double ObtenerAnguloOpuestoA()
+        {
+            return anguloOpuestoA;
+        }
+
+        public double ObtenerAnguloOpuestoB()
+        {
+            return 90 - anguloOpuestoA;
+        }
+
+        public double ObtenerArea()
+        {
+            return catetoA * ObtenerCatetoB() / 2;
+        }
+    }
+
+    class Triangulo
+    {
+        static void Main()
+        {
+            Console.Write("Ingrese la longitud del cateto A (en metros): ");
+            double catetoA = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el ángulo opuesto al cateto A (en grados): ");
+            double anguloOpuestoA = double.Parse(Console.ReadLine());
+
+            TrianguloRectangulo objTriangulo = new TrianguloRectangulo(catetoA, anguloOpuestoA);
+
+            Console.WriteLine("Resultados:");
+            Console.WriteLine("Cateto A:" + objTriangulo.ObtenerCatetoA() + " metros");
+            Console.WriteLine("Cateto B:" + objTriangulo.ObtenerCatetoB() + "metros");
+            Console.WriteLine("Hipotenusa:" + objTriangulo.ObtenerHipotenusa() + "metros");
+            Console.WriteLine("Ángulo opuesto de A:" + objTriangulo.ObtenerAnguloOpuestoA() + "grados");
+            Console.WriteLine("Ángulo opuesto de B: " + objTriangulo.ObtenerAnguloOpuestoB() + "grados");
+            Console.WriteLine("Área" + objTriangulo.ObtenerArea() + "metros cuadrados");
+        }
+    }
+
+}
+        
+   
+
