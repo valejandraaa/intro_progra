1c2f1e54a028fb8ee8e93b6b84af52b2ad150ec9
valejandraaa <142243750+valejandraaa@users.noreply.github.com>
2023-10-30 21:29:42 -0600

    Add files via upload

---
+++ SEMANA_13_VALERIA URIZAR_1002322/L13VAUM1007322/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L13VAUM1007322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] notas = new double[8];
+            double promedio = 0;
+            double suma = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine("Ingrese la nota para el índice " + i);
+                notas[i] = double.Parse(Console.ReadLine());
+                suma = suma + notas[i];
+            }
+
+            promedio = suma / 8;
+            double notaMasAlta = notas[0];
+            for (int i = 1; i < notas.Length; i++)
+            {
+                if (notas[i] > notaMasAlta)
+                {
+                    notaMasAlta = notas[i];
+                }
+            }
+            Console.WriteLine("La nota más alta es: " + notaMasAlta);
+            Console.WriteLine("Promedio: " + promedio);
+
+
+
+            Console.WriteLine("Ingrese una palabra");
+            string palabra = Console.ReadLine();
+            Console.WriteLine("Ingrese una letra: ");
+            char letra = char.Parse(Console.ReadLine());
+            int contador = 0;
+            bool existe = false;
+
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                if (palabra[i] == letra)
+                {
+                    contador++;
+                }
+                if (i == 0 && palabra[i] == letra)
+                {
+                    existe = true;
+                }
+            }
+            if (existe)
+            {
+                Console.WriteLine("La letra ingresada existe al inicio de la palabra");
+            }
+            else
+            {
+                Console.WriteLine("La letra ingresada no existe al inicio de la palabra");
+            }
+            Console.WriteLine("La cantidad de veces que se encuentra la letra en la palabra es:" + contador);
+            Console.ReadKey();
+        }
+    } 
+    
+    
+    
---
+++ SEMANA_13_VALERIA URIZAR_1002322/L13VAUM1007322/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("L13VAUM1007322")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("L13VAUM1007322")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("b098ba9c-a708-4be5-a18c-2cb21c086c25")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
